d573bf3ca5854b5022509442cd7392c08f98cf5c
Aziem Chawdhary <aziem.chawdhary@gmail.com>
2021-10-26 20:53:14 +0100

    Add IntelliJ Community to dev env

--- legend.nix
+++ legend.nix
@@ -1,6 +1,11 @@
 with (import <nixpkgs> {});
 mkShell {
 	buildInputs = with pkgs; [
-		jdk11 maven nodejs-14_x yarn
+		jdk11 maven nodejs-14_x yarn jetbrains.idea-community
 	];
+
+	shellHook = ''
+		export JAVA_HOME="${pkgs.jdk11}/lib/openjdk"
+		'';
+
 }
